8a470ee1269c3dfbd8e25a1f617685e41e1549cd
Kainanthyz <santos.blanzgamer@gmail.com>
2025-09-09 20:51:18 -0300

    QB-17-feat(tickets-by-product):Implementation of the endpoint for searching tickets by product

--- repositories/tickets_repository.py
+++ repositories/tickets_repository.py
@@ -1,6 +1,6 @@
 from config.db_connection import get_cursor
 
-class TicketsByCompanyRepository:
+class TicketsRepository:
     def get_tickets_by_company(self):
         """
         Executa a consulta no banco de dados para contar os tickets por empresa.
@@ -18,6 +18,27 @@ class TicketsByCompanyRepository:
                 c.name;
         """
         
+        with get_cursor() as cur:
+            cur.execute(sql_query)
+            return cur.fetchall()
+        
+    def get_tickets_by_product(self):
+        """
+        Executa a consulta no banco de dados para contar os tickets por produto.
+        Retorna uma lista de tuplas.
+        """
+        sql_query = """
+            SELECT
+                p.name,
+                COUNT(t.ticketid) AS ticket_count
+            FROM
+                tickets t
+            JOIN
+                products p ON t.productid = p.productid
+            GROUP BY
+                p.name;
+        """
+
         with get_cursor() as cur:
             cur.execute(sql_query)
             return cur.fetchall()
