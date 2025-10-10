0ac2deaf78f3bf9e459fde74e7dc7b74a050c5f2
André Meneses <andremeneses0103@gmail.com>
2025-10-09 21:55:49 -0300

    QB-70-feat(service): created service to get products data

---
+++ services/products_service.py
@@ -0,0 +1,20 @@
+from repositories.products_repository import ProductsRepository
+from utils.encryptor import decrypt_data
+
+class ProductsService:
+    def __init__(self):
+        self.products_repository = ProductsRepository()
+    
+    def get_all_products(self):
+        """
+        Retorna todos os produtos.
+        """
+        results = self.products_repository.get_all_products()
+        products = []
+        for row in results:
+            product = {
+                "ProductID": row[0],
+                "ProductName": row[1]
+            }
+            products.append(product)
+        return products
