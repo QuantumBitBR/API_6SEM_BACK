b0437c2a2170d7cf16514298cacf6abae65dd83b
santosjhony12 <santosdesouzajhony@gmail.com>
2025-10-09 22:47:56 -0300

    QB-60-feat(IA): created endpoint to get predict data

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from controllers.user_controller import users_ns
 from config.extensions import db, ma, cache
 from controllers.companies_controller import companies_ns
 from controllers.privacy_policy_controller import privacy_policy_ns
+from controllers.ia_controller import ia_ns
 from controllers.user_controller import users_ns
 from flask_caching import Cache
 from dotenv import load_dotenv
@@ -21,6 +22,7 @@ CORS(app)
 api.add_namespace(tickets_ns, '/tickets')
 api.add_namespace(users_ns, "/user")
 api.add_namespace(auth_ns, "/auth")
+api.add_namespace(ia_ns, '/ia')
 # Database config
 app.config['SECRET_KEY'] = environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
